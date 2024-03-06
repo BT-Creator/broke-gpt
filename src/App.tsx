714f8562fcb710c92940cb93ff39e6ed4f72fd91
Bo Robbrecht <robbrechtbo@hotmail.com>
2024-03-06 18:30:30 +0100

    Added input field

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
+import { MdSend } from 'react-icons/md'
 import './App.css'
-import { AppShell, Text } from '@mantine/core'
+import { ActionIcon, Affix, Group, TextInput } from '@mantine/core'
+
+
 
 function App() {
+
+
   return (
-    <AppShell>
-      <AppShell.Header>
-        <Text>Broke GPT</Text>
-      </AppShell.Header>
-      <AppShell.Main>
-        <Text>Chat window</Text>
-      </AppShell.Main>
-    </AppShell>
+    <>
+      <Affix position={{bottom:0, left: 0}}>
+        <Group justify='space-between' w='100vw' px='5vw' py='lg' bg='gray'>
+          <TextInput style={{flexGrow: 1}}/>
+          <ActionIcon size='xl'>
+            <MdSend />
+          </ActionIcon>
+        </Group>
+      </Affix>
+    </>
   )
 }
 
